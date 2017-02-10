733d5e38a6b51266b232696ade4cdcc798445319
j-cira <j-cira@onu.edu>
2017-02-10 01:19:26 -0500

    Add files via upload

---
+++ Magazine Copy/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Magazine Ad</title>
+        <LINK href="MagazineStyle.css" rel="stylesheet" type="text/css">
+        <LINK rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Geo" >
+    </head>
+    <body link="white">
+        <div id="container">
+            <h1 id="firstText">
+                <p>ORANGE YOU GLAD</p>
+                <p>TO SEE ME?</p>
+            </h1>
+            <div>
+                <img src="Converse-Shoe.png" width="520px">
+            </div>
+            <h2 id="secondText">
+                <p>MORE THAN JUST A SHOE.</p>
+                <p>EXPRESS YOU.</p>
+            </h2>
+            <h3 id="thirdText">
+                <p>WITH THE CONVERSE SNEAKER CREATOR YOU CAN</p>
+                <p>CREATE YOUR VERY OWN SHOE UNIQUELY FOR YOU!</p>
+                <p>GO TO <a href="http://www.converse.com">CONVERSE.COM</a></p>
+            </h3>
+            <div>
+                <img src="Converse-Logo.png">
+            </div>
+        </div>
+    </body>
+</html>
